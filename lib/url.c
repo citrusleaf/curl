6631200a9555d59436422f229bcaec8bf898bb5b
Sunil Sayyaparaju <sunil@aerospike.com>
2018-06-05 11:14:19 +0530

    Remove pipelining of POST request which does not gel well with http-continue

--- lib/url.c
+++ lib/url.c
@@ -836,7 +836,6 @@ static int IsPipeliningPossible(const struct Curl_easy *handle,
     if(Curl_pipeline_wanted(handle->multi, CURLPIPE_HTTP1) &&
        (handle->set.httpversion != CURL_HTTP_VERSION_1_0) &&
        (handle->set.httpreq == HTTPREQ_GET ||
-        handle->set.httpreq == HTTPREQ_POST ||
         handle->set.httpreq == HTTPREQ_HEAD))
       /* didn't ask for HTTP/1.0 and a GET or HEAD */
       avail |= CURLPIPE_HTTP1;
